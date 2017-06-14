fb1bfc99e1e285d11a2ca30a0d2cc5a0d50d0e70
AMIR\amir <choubaniAmir@bitbucket.org>
2017-06-14 23:34:34 +0200

    aaaa

---
+++ ModernGUIDevelopment_vidéo2/Assets/_scripts/buttonTest.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class buttonTest : MonoBehaviour {
+
+    
+
+    public void doSomeThing(Slider slider)
+    {
+        Debug.Log(slider.value.ToString());
+    }
+}
